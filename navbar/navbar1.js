860ee3e5b7ea9cb24e04778fc0ea4c757f954ea2
Peter English <englishpeter29@gmail.com>
2021-10-06 13:04:51 +0100

    added media queries and a responsive navbar

---
+++ navbar/navbar1.js
@@ -0,0 +1,20 @@
+//This file is just about adding and removing classes when the hamburger is clicked
+//You get the navMenu to swoop in from left: -100%
+//And you get the hamburger to toggle its active and inactive appearances.
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
